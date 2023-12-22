2f6bd4bc54f82c5870d737cf295c3d3005b88e90
Shishir Bahuguna <shishir.bahuguna@cynoteck.com>
2023-12-22 15:24:55 +0530

    ENV checked

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,5 @@
-import { config } from "@/utils/config";
-
 export async function GET(request: Request) {
-  console.log("config.db_uri");
-  console.log(config.db_uri);
-  console.log("config.db_uri");
   return Response.json({
-    message: config.db_uri,
+    message: "Welcome to Open-LSM api",
   });
 }
